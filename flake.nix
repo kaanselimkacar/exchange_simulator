99617141be37b0e843d6aa78fe93ad014f550cbb
git <kaan1410141@gmail.com>
2026-05-16 14:45:40 +0200

    refactor: shell-nix removed, flakes added

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  outputs = {nixpkgs, ...}: let
+    system = "x86_64-linux";
+  in {
+    devShells.${system}.default = nixpkgs.legacyPackages.${system}.mkShell {
+      packages = with nixpkgs.legacyPackages.${system}; [
+        clang
+        clang-tools
+        cmake
+      ];
+    };
+  };
+}
